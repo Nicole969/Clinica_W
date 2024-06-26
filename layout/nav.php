4b9cf8f59c39e7ff5a6437a034f0fa0fc3043779
Erickmela <2021110411@udh.edu.pe>
2024-06-26 13:47:09 -0500

    Welcome

---
+++ layout/nav.php
@@ -0,0 +1,11 @@
+<header class="text-black py-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <h1 class="text-lg sm">DuoDent</h1>
+        <div class="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-4">
+            <a href="#" class="text-black px-4 py-2 rounded hover:text-blue-600">Registro</a>
+            <div>
+                <a href="#" class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 block md:inline-block">Iniciar Sesión</a>
+            </div>
+        </div>
+    </div>
+</header>
