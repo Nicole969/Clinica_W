1e2b188326622b33970c354c41806822278364c0
Erickmela <2021110411@udh.edu.pe>
2024-06-26 20:45:06 -0500

    Tablas de admin

--- dashboard.php
+++ dashboard.php
@@ -35,8 +35,39 @@ require_once 'layout/nav.php';
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
                 <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-                    <dt class="text-base leading-7 text-gray-600">Transactions every 24 hours</dt>
-                    <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">44 million</dd>
+                    <dt class="text-base leading-7 text-gray-600">Usuarios registrados</dt>
+                    
+                    <?php
+                    // Establecer conexión a la base de datos (ejemplo básico)
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "bdclinica";
+
+                    // Crear conexión
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+
+                    // Verificar conexión
+                    if ($conn->connect_error) {
+                        die("Conexión fallida: " . $conn->connect_error);
+                    }
+
+                    // Consulta SQL para obtener la cantidad de usuarios registrados
+                    $sql = "SELECT COUNT(*) AS total_usuarios FROM user";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        // Obtener el resultado como arreglo asociativo
+                        $row = $result->fetch_assoc();
+                        $total_usuarios = $row['total_usuarios'];
+                    } else {
+                        $total_usuarios = 0; // Si no hay resultados, se establece a cero
+                    }
+
+                    // Cerrar conexión
+                    $conn->close();
+                ?>
+                    <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl"><?php echo $total_usuarios; ?></dd>
                 </div>
                 <div class="mx-auto flex max-w-xs flex-col gap-y-4">
                     <dt class="text-base leading-7 text-gray-600">Assets under holding</dt>
@@ -49,75 +80,108 @@ require_once 'layout/nav.php';
             </dl>
         </div>
     </div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 p-4 w-full">
+
+        <!-- Pacientes y Medicos -->
         <div class="p-6 bg-gray-100">
-            <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-8 ">
-                <h2 class="text-2xl font-bold mb-4">Example Datatable</h2>
-                <table id="example" class="table-auto w-full">
-                    <thead>
-                        <tr>
-                            <th class="px-4 py-2">Asunto</th>
-                            <th class="px-4 py-2">Descripcion</th>
-                            <th class="px-4 py-2">Fecha</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($citas as $cita) : ?>
+            <div class="text-center flex gap-x-16 justify-center">
+            <!-- Lista de pacientes -->
+            <div class="mb-6">
+                <h2 class="text-xl mb-4">Lista de Pacientes</h2>
+                <div class="overflow-x-auto">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-black">
                             <tr>
-                                <td class="border px-4 py-2"><?php echo $cita["Asunto"] ?></td>
-                                <td class="border px-4 py-2"><?php echo $cita["Descripcion"] ?></td>
-                                <td class="border px-4 py-2"><?php echo $cita["Fecha"] ?></td>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Usuario</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Correos</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Tipo</th>
                             </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            <?php foreach ($pacientes as $paciente) : ?>
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap"><?php echo $paciente["Username"] ?></td>
+                                    <td class="px-6 py-4 whitespace-nowrap"><?php echo $paciente["Correo"] ?></td>
+                                    <td class="px-6 py-4 whitespace-nowrap"><?php echo $paciente["tipo"] ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-        <div class="p-6 bg-gray-100">
-            <div class="p-2 bg-gray-100">
-                <h2>Lista de pacientes</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Usuario</th>
-                            <th>Correos</th>
-                            <th>Tipo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($pacientes as $paciente) : ?>
+
+            <!-- Lista de medicos -->
+            <div class="">
+                <h2 class="text-xl mb-4">Lista de Médicos</h2>
+                <div class="overflow-x-auto">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-black">
                             <tr>
-                                <td><?php echo $paciente["Username"] ?></td>
-                                <td><?php echo $paciente["Correo"] ?></td>
-                                <td><?php echo $paciente["tipo"] ?></td>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Usuario</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Correos</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Tipo</th>
                             </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            <?php foreach ($medicos as $medico) : ?>
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap"><?php echo $medico["Username"] ?></td>
+                                    <td class="px-6 py-4 whitespace-nowrap"><?php echo $medico["Correo"] ?></td>
+                                    <td class="px-6 py-4 whitespace-nowrap"><?php echo $medico["tipo"] ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div class="p-2 bg-gray-100">
-                <h2>Lista de Medicos</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Usuario</th>
-                            <th>Correos</th>
-                            <th>Tipo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($medicos as $medico) : ?>
-                            <tr>
-                                <td><?php echo $medico["Username"] ?></td>
-                                <td><?php echo $medico["Correo"] ?></td>
-                                <td><?php echo $medico["tipo"] ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
             </div>
-        </div>
+            
+            <div class="p-6 bg-gray-100">
+                <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+                    <div class="flex items-center justify-between mb-4">
+                        <h2 class="text-2xl">Citas Pendientes</h2>
+                        <input type="text" id="searchInput" onkeyup="searchTable()" placeholder="Buscar..." class="p-2 border rounded">                
+                    </div>
+
+                    <div class="overflow-x-auto">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-black">
+                                <tr>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Estado</th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Asunto</th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Descripcion</th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Hora</th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Fecha</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                <?php foreach ($citas as $cita) : 
+                                    $estadoClass = '';
+                                    if ($cita["Estado"] == "Pendiente") {
+                                        $estadoClass = 'bg-yellow-300 text-yellow-600';
+                                    } elseif ($cita["Estado"] == "Confirmada") {
+                                        $estadoClass = 'bg-green-300 text-green-600';
+                                    }
+                                ?>
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap<?php echo $estadoClass ?>"><?php echo $cita["Estado"] ?></td>
+                                        <td class="px-6 py-4 whitespace-nowrap"><?php echo $cita["Asunto"] ?></td>
+                                        <td class="px-6 py-4 whitespace-nowrap"><?php echo $cita["Descripcion"] ?></td>
+                                        <td class="px-6 py-4 whitespace-nowrap"><?php echo $cita["Fecha"] ?></td>
+                                        <td class="px-6 py-4 whitespace-nowrap"><?php echo $cita["Hora"] ?></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
     </div>
+
+
+
+
+    
 
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
@@ -129,6 +193,9 @@ require_once 'layout/nav.php';
                 // Add any customization options here
             });
         });
+
+
+        
     </script>
 
 </div>
