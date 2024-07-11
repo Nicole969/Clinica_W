cac43016bd54435e9a02268a06a9a0d37264b696
Erickmela <2021110411@udh.edu.pe>
2024-07-10 19:02:14 -0500

    Citas programadas

--- medicos.php
+++ medicos.php
@@ -2,6 +2,8 @@
 // Incluir controladores necesarios
 require_once "models/Horarios.php";
 require_once "controllers/HorarioController.php";
+require_once "controllers/CitaController.php";
+
 
 // Iniciar sesión
 session_start();
@@ -21,6 +23,11 @@ if ($_SESSION["tipo"] == "paciente") {
 $id = $_SESSION["id"];
 $hora = new HorarioController();
 $horarios = $hora->mostrarMisHorarios($id);
+
+// Instanciar el controlador de Citas y obtener los datos
+$citasController = new CitasController();
+$citasMedico = $citasController->mostrarCitasMedico($id);
+
 
 // Formulario de horario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -46,23 +53,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     require_once 'layout/nav.php';
     ?>
 
-<main class="bg-cover bg-center bg-gradient-to-b from-blue-300 to-white bg-opacity-75 py-3 md:py-6 h-screen px-4">
-        <div class="flex justify-end">
-            <div class="bg-white shadow-md rounded-lg p-6 max-w-md mx-4">
+    <main class="bg-cover bg-center bg-gradient-to-b from-blue-300 to-white bg-opacity-75 py-3 md:py-6 h-screen px-8">
+        <div class="flex flex-col justify-between space-y-4">
+            <!-- Div de Horarios -->
+            <div class="p-6 max-w-full mx-4 mb-4">
                 <?php if ($_SESSION["tipo"] == "medico"): ?>
-                    <!-- Botón del formulario -->
-                    <div class="flex justify-end mb-4">
-                        <div onclick="mostrarFormularioHorario()" class="bg-blue-500 hover:bg-blue-700 text-white text-sm py-2 px-3 rounded-md cursor-pointer transition transform hover:-translate-y-1">
+                    <div class="flex items-center mb-4">
+                        <h2 class="text-xl mr-4 text-black">Mis Horarios</h2>
+                        <!-- Botón del formulario -->
+                        <div onclick="mostrarFormularioHorario()" class="bg-black hover:bg-black text-white text-sm py-2 px-3 rounded-md cursor-pointer transition transform hover:-translate-y-1">
                             Ingresar Horario
                         </div>
                     </div>
+                <?php else: ?>
+                    <h2 class="text-xl mb-4">Mis Horarios</h2>
                 <?php endif; ?>
 
-                <h2 class="text-xl mt-4 mb-4">Mis Horarios</h2>
-                <div class="overflow-x-auto">
+                <!-- Horarios -->
+                <div class="flex flex-wrap overflow-x-auto">
                     <?php foreach ($horarios as $index => $horario) : ?>
                         <!-- Modal para cada horario -->
-                        <div class="bg-white p-8 w-full relative border-t-4 border-blue-500">
+                        <div class="bg-white rounded-lg p-8 w-full sm:w-auto sm:mr-4 mb-4 relative border-blue-500 cursor-pointer transition transform hover:-translate-y-1">
                             <div class="flex flex-col items-start">
                                 <p class="text-md"><strong>Día:</strong> <?php echo $horario["diaSemana"]; ?></p>
                                 <p class="text-md"><?php echo $horario["horaInicio"] . ' - ' . $horario["horaFin"]; ?></p>
@@ -79,7 +90,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                             </svg>
                         </button>
-                        
+
                         <form action="" method="post" class="space-y-4 mt-8">
                             <h2 class="text-2xl text-center text-gray-800 mb-8">Ingresar Horario</h2>
 
@@ -92,7 +103,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                     <option value="Jueves">Jueves</option>
                                     <option value="Viernes">Viernes</option>
                                     <option value="Sábado">Sábado</option>
-                                    <option value="Domingo">Domingo</option>
                                 </select>
                             </div>
 
@@ -107,7 +117,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             </div>
 
                             <input type="hidden" name="id_user" value="<?php echo $_SESSION['id']; ?>">
-                            
+
                             <div class="flex justify-center mt-8">
                                 <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md cursor-pointer transition transform hover:-translate-y-1">
                                     Guardar Horario
@@ -118,10 +128,68 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     </div>
                 </div>
             </div>
+
+            <!-- Div de Citas -->
+            <div class="bg-white shadow-md rounded-lg p-6 mx-4 max-w-full relative">
+                <div class="flex items-center justify-between">
+                    <h2 class="text-xl mt-4 mb-4">Citas Programadas</h2>
+
+                    <!-- Filtro de Estado -->
+                    <div class="relative">
+                        <button id="filtroEstadoBtn" class="flex items-center text-gray-600 hover:text-gray-900 focus:outline-none">
+                            <svg class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4h14a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1z"></path>
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l4 4 4-4m0 6H4"></path>
+                            </svg>
+                            Filtro
+                        </button>
+
+                        <!-- Menú desplegable del filtro -->
+                        <div id="filtroEstadoMenu" class="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg hidden z-10">
+                            <div class="py-1">
+                                <button onclick="filtrarCitas('Pendiente')" class="block text-left w-full px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white focus:outline-none">Pendiente</button>
+                                <button onclick="filtrarCitas('Confirmado')" class="block text-left w-full px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white focus:outline-none">Confirmado</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="overflow-x-auto">
+                    <?php foreach ($citasMedico as $cita) : ?>
+                        <div class="bg-white p-4 w-full relative border-t-2 border-1 border-black mb-4 cita-item flex justify-center">
+                            <!-- Estado de la cita -->
+                            <div class="flex flex-row items-center mb-4 space-x-20 justify-center">
+                                <div class="text-white p-2 rounded-md <?php echo ($cita['Estado'] == 'Pendiente') ? 'bg-gray-400' : 'bg-green-400'; ?>">
+                                    <p class="text-xs"><?php echo $cita['Estado']; ?></p>
+                                </div>
+
+                                <div class="ml-4 flex flex-row justify-center items-center space-x-20">
+                                    <div class="flex flex-col space-y-4">
+                                        <p class="text-sm"><strong>Paciente: </strong><?php echo $cita['NombrePaciente']; ?></p>
+                                        <p class="text-sm"><strong>Correo: </strong><?php echo $cita['CorreoPaciente']; ?></p>
+                                    </div>
+
+                                    <div class="space-y-4">
+                                        <p class="text-sm"><strong>Fecha:</strong> <?php echo $cita['Start']; ?></p>
+                                        <p class="text-sm"><strong>Hora:</strong> <?php echo $cita["Hora_Inicial"] . ' - ' . $cita["Hora_Final"]; ?></p>
+                                    </div>
+
+                                    <div class="space-y-4">
+                                        <p class="text-sm"><strong>Información:</strong> <?php echo $cita['Title']; ?> </p>
+                                        <p class="text-sm"><strong>Descripción:</strong> <?php echo $cita['Descripcion']; ?> </p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
         </div>
     </main>
 
     <script>
+
          function mostrarFormularioHorario() {
             var modal = document.getElementById('formularioHorarioModal');
             modal.style.display = 'flex'; // Mostrar el modal
@@ -131,7 +199,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             var modal = document.getElementById('formularioHorarioModal');
             modal.style.display = 'none'; // Ocultar el modal
         }
+
+        // Función para filtrar citas por estado
+        function filtrarCitas(estado) {
+            const citas = document.querySelectorAll('.cita-item');
+            
+            citas.forEach(cita => {
+                const estadoCita = cita.querySelector('.rounded-md').textContent.trim();
+                
+                if ((estado === 'Pendiente' && estadoCita !== 'Pendiente') || (estado === 'Confirmado' && estadoCita !== 'Confirmado')) {
+                    cita.style.display = 'none';
+                } else {
+                    cita.style.display = 'block';
+                }
+            });
+        }
+
+        // Mostrar y ocultar menú desplegable de filtro
+        const filtroEstadoBtn = document.getElementById('filtroEstadoBtn');
+        const filtroEstadoMenu = document.getElementById('filtroEstadoMenu');
+
+        filtroEstadoBtn.addEventListener('click', () => {
+            filtroEstadoMenu.classList.toggle('hidden');
+        });
+
+        // Cerrar menú desplegable cuando se hace clic fuera de él
+        document.addEventListener('click', (event) => {
+            if (!filtroEstadoBtn.contains(event.target) && !filtroEstadoMenu.contains(event.target)) {
+                filtroEstadoMenu.classList.add('hidden');
+            }
+        });
+        
     </script>
+
 </body>
 
 
